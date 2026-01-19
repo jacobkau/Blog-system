3c7d8a35c4813cafaacca65f304aa6512e63e1ee
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2026-01-19 13:29:58 +0200

    Update categories.js

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,14 +1,22 @@
 import express from 'express';
 import {
   getCategories,
-  createCategory
+  getCategory,
+  createCategory,
+  updateCategory,
+  deleteCategory
 } from '../controllers/categories.js';
+import { protect } from '../middleware/auth.js'; // Import protect middleware
 
 const router = express.Router();
 
-
 router.route('/')
-  .get(getCategories)     
-  .post(createCategory);    
+  .get(getCategories)
+  .post(protect, createCategory); // Protected route
+
+router.route('/:id')
+  .get(getCategory)
+  .put(protect, updateCategory)    // Protected route
+  .delete(protect, deleteCategory); // Protected route
 
 export default router;
